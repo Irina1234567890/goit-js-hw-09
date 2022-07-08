7e00b0b625f5d28a832ddf4be04930a71c82eb33
Irina1234567890 <irina3213@gmail.com>
2022-07-08 18:24:48 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+const colors = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+}
+
+let intervalId = null;
+
+colors.startBtn.addEventListener('click', onStartBtnClick);
+colors.stopBtn.addEventListener('click', onStopBtnClick);
+
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick(event) {
+  event.preventDefault();
+  intervalId = setInterval(() => {
+      colors.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  colors.startBtn.disabled = true;
+  colors.startBtn.removeEventListener('click', onStartBtnClick);
+};
+
+function onStopBtnClick(event) {
+  event.preventDefault();
+  clearInterval(intervalId);
+
+  colors.startBtn.disabled = false;
+  colors.startBtn.addEventListener('click', onStartBtnClick);
+   
+}
